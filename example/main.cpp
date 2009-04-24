3e69293e69e5f5008aec0f08513eec15c08487c5
amyasnikov <amyasnikov@f122bb90-0c13-4a90-be39-cbef6db7cf7f>
2009-04-24 05:02:42 +0000

    Change example application dialog
    git-svn-id: https://svn.code.sf.net/p/jfuzzyqt/code@72 f122bb90-0c13-4a90-be39-cbef6db7cf7f

--- example/main.cpp
+++ example/main.cpp
@@ -7,8 +7,6 @@
 
 int main(int, char**)
 {
-    char	option;	// var for selection of what user wants to do
-
     std::cout.setf(std::ios::fixed);
     std::cout.precision(2); // only display 2 decimal places
     // create and load the model
@@ -36,21 +34,37 @@ int main(int, char**)
         // is set to the executable's directory if running from the MSVC IDE
         return 0;
     }
-    std::cout << "SELECT AN OPTION (S - set values, Q - quit) : ";
-    std::cin >> option;
-    if (option == 'S' || option == 's')
+    int service, food; // values for input variables
+    std::cout << "                                food                               " << std::endl;
+    std::cout << " |     |-----------------------------------------------------------" << std::endl;
+    std::cout << " |value| 0.0 | 1.0 | 2.0 | 3.0 | 4.0 | 5.0 | 6.0 | 7.0 | 8.0 | 9.0 " << std::endl;
+    std::cout << "-|-----------------------------------------------------------------" << std::endl;
+    for(service = 0; service < 10; service++)
     {
-        float service, food; // values for input variables
-        for(service = 0; service < 10; service++)
+        QString line;
+        switch(service)
         {
-            for(food = 0; food < 10; food++)
-            {
-                model.setVariable("service", service);
-                model.setVariable("food", food);
-                model.evaluate();
-                std::cout << "f(" << service << ";" << food << ")=" << model.getValue("tip") << std::endl;
-            }
+            case 1: line.append("s"); break;
+            case 2: line.append("e"); break;
+            case 3: line.append("r"); break;
+            case 4: line.append("v"); break;
+            case 5: line.append("i"); break;
+            case 6: line.append("c"); break;
+            case 7: line.append("e"); break;
+            default: line.append(" "); break;
         }
-    } // end if option = 's'
+        line.append("|");
+        line.append(QString("%1").arg(double(service),5,'f',2));
+        line.append("|");
+        for(food = 0; food < 10; food++)
+        {
+            model.setVariable("service", service);
+            model.setVariable("food", food);
+            model.evaluate();
+            line.append(QString("%1").arg(model.getValue("tip"),5,'f', 2));
+            if(food < 9) line.append("|");
+        }
+        std::cout << line.toLocal8Bit().data() << std::endl;
+    }
     return 0;
-} // end main()
+}
